5feb37d5d6c3247fe3d5f8f3e4006d319fbb9cc3
Ryhor Volsasu <g.volosov@email.com>
2024-03-09 16:15:54 +0300

    feat: add findAll and findOne methods for tracks

--- src/tracks/tracks.service.ts
+++ src/tracks/tracks.service.ts
@@ -1,19 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { data } from 'src/data/data';
+import { validate } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class TracksService {
+  findAll() {
+    return data.tracks;
+  }
+
+  findOne(id: string) {
+    if (!validate(id)) throw new BadRequestException('Invalid id (not uuid)');
+    const artist = data.tracks.find((track) => track.id === id);
+    if (!artist) {
+      throw new NotFoundException('Not found artist');
+    }
+    return artist;
+  }
   create(createTrackDto: CreateTrackDto) {
     return 'This action adds a new track';
-  }
-
-  findAll() {
-    return `This action returns all tracks`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} track`;
   }
 
   update(id: number, updateTrackDto: UpdateTrackDto) {

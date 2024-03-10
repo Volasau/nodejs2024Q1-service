0ad14cc91e6b9f09a76551e23b63826a3bf32b4f
Ryhor Volsasu <g.volosov@email.com>
2024-03-11 00:57:36 +0300

    feat: add swagger and correct api.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { readFile } from 'fs/promises';
+import * as YAML from 'yaml';
+import { SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
-const PORTnest = Number(process.env.PORT);
+const PORTnest = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const fileSchema = await readFile('./doc/api.yaml', 'utf8');
+  const docSchema = YAML.parse(fileSchema);
+  SwaggerModule.setup('doc', app, docSchema);
   await app.listen(PORTnest);
 }
 bootstrap();

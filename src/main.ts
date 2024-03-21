a8e3ad0ab51b152ab912447843fe7d83e1508372
Ryhor Volsasu <g.volosov@email.com>
2024-03-21 14:58:39 +0300

    feat: add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,23 @@ import * as dotenv from 'dotenv';
 import { readFile } from 'fs/promises';
 import * as YAML from 'yaml';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 const PORTnest = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const fileSchema = await readFile('./doc/api.yaml', 'utf8');
   const docSchema = YAML.parse(fileSchema);
   SwaggerModule.setup('doc', app, docSchema);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, forbidUnknownValues: true }),
+  );
+
   await app.listen(PORTnest);
 }
+
 bootstrap();

2e5ce65b7660bec724c546cb9bc7658694bd38f1
Ryhor Volsasu <g.volosov@email.com>
2024-03-21 16:15:07 +0300

    refactor: change app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
-import { AlbumsModule } from './albums/albums.module';
 import { TracksModule } from './tracks/tracks.module';
+import { AlbumsModule } from './albums/albums.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import configService from './ormconfig';
-import { FavsModule } from './favorites/favs/favs.module';
-import { FavArtistsModule } from './favorites/fav-artists/fav-artists.module';
-import { FavTracksModule } from './favorites/fav-tracks/fav-tracks.module';
-import { FavAlbumsModule } from './favorites/fav-albums/fav-albums.module';
-import { RouterModule } from '@nestjs/core';
-import { favsRoutes } from './favorites/favs.routes';
+import { database } from './db/ormconfig';
 
 @Module({
   imports: [
@@ -20,12 +16,11 @@ import { favsRoutes } from './favorites/favs.routes';
     ArtistsModule,
     AlbumsModule,
     TracksModule,
-    FavsModule,
-    FavArtistsModule,
-    FavTracksModule,
-    FavAlbumsModule,
-    RouterModule.register([...favsRoutes]),
-    TypeOrmModule.forRoot(configService),
+    FavoritesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => database,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
